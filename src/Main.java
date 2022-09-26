f4dd47f56108bd515748639c93cd9ed5cc5ab7a9
Raymond Ha <Haro@mail.uc.edu>
2022-09-26 03:10:28 -0400

    added numbers and strings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        int stringNumber1;
+        int stringNumber2;
+        String notLetter;
+
+        stringNumber1= ;
+        stringNumber2= 0;
+        
+
+        if(stringNumber1>stringNumber2)
+        {
+            System.out.println(stringNumber1 + " is greater than " + stringNumber2);
+        }
+        else if (stringNumber1<stringNumber2)
+        {
+            System.out.println(stringNumber1 + " is less than " + stringNumber2);
+        }
+        else if (stringNumber1==stringNumber2)
+        {
+            System.out.println(stringNumber1 + " is tied with " + stringNumber2);
+        }
+        else
+        {
+            System.out.println("You didn't enter a number. Try again.");
+        }
     }
 }
